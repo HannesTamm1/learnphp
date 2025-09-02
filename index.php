3c2727f2fbc7dd3f36051805c8bcd20a173fa9cc
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-09-02 11:32:23 +0300

    Add arrays

--- index.php
+++ index.php
@@ -1,5 +1,40 @@
 <?php
 
+//Array
+$array = array(1, 2, 3, 4);
+$array = [1, 2, 3, 4];
+$array = [
+    1,
+    "Hello",
+    true,
+    null,
+    [1, 2, 3]
+];
+$array = [
+    'name' => 'Hannes',
+    'age' => 18,
+    'gender' => 'male',
+    'location' => 'Estonia',
+    69 => "idk",
+    1,
+    2,
+    3
+];
+var_dump($array['name']);
+$array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+//Add array values
+var_dump($array[1][1]);
+$array = [1, 2 ,3];
+array_push($array, 4);
+var_dump($array);
+
+
+// TYPES
+/*
 echo "Hello, World!\n";
 
 
@@ -16,3 +51,5 @@ $bar;
 $bar = NULL;
 var_dump($num);
 var_dump($bar);
+*/
+
