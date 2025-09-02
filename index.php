f1689c78a7775923110f3c82983453758c570651
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-09-02 11:14:06 +0300

    Add type examples

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello, World!\n";
+
+
+$var = "Ära mine metsa \n";
+echo $var;
+
+
+$num = 2;
+$num *=2;
+$num **=3;
+$num = 13;
+$num %= 5;
+$bar;
+$bar = NULL;
+var_dump($num);
+var_dump($bar);
