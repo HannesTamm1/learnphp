0eceeff7a5a7c0e955c74cac5de45571f3e19103
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-09-19 12:08:40 +0300

    Add blog template

--- index.php
+++ index.php
@@ -1,43 +1,17 @@
-<?php
+<?php include __DIR__ . '/partials/header.php'; ?>
 
-class Cat {
-    public function __construct() {
-        var_dump('Class was created');
-    }
 
-    public function __call($name, $args) {
-        var_dump($name, $args);
-    }
+    <main class="container">
+      <?php include __DIR__ . '/partials/hero.php'; ?>
+      <?php include __DIR__ . '/partials/featured.php'; ?>
 
-    public function __toString(){
-        return 'Mjäu';
-    }
-
-    public function __destruct(){
-        var_dump("The class was destored");
-    }
-
-    public function __get($name){
-        var_dump($name);
-        return 'Hell yeah';
-    }
-
-    public function __set($name, $value){
-        var_dump($name, $value);
-        
-    }
-    
-}
-
-function makeCat(){
-    $cat = new Cat();
-}
-
-$kitty = new Cat();
-var_dump($kitty);
-var_dump($kitty->mood);
-$kitty -> color = 'Rainbow';
-$kitty->throwShit('Many', 'smelly', 'why');
-echo $kitty;
-$kitty = 1;
-var_dump("Something");
+      <div class="row g-5">
+        <div class="col-md-8">
+            <?php include __DIR__ . '/partials/posts.php'; ?>
+        </div>
+        <div class="col-md-4">
+          <?php include __DIR__ . '/partials/sidebar.php'; ?>
+        </div>
+      </div>
+    </main>
+<?php include __DIR__ . '/partials/footer.php'; ?>
