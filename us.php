06f7999b3e1abe288064c84d4d39838266d19fb2
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-09-19 13:49:14 +0300

    old site example

--- us.php
+++ us.php
@@ -1,39 +1,33 @@
 <?php
 $posts = [
   [
-    'title' => 'World news 1',
+    'title' => 'U.S news 1',
     'published' => '16.09.2025',
     'author' => 'Hannes',
-    'body' => 'Some world news body 1'
+    'body' => 'Some U.S news body 1'
   ],
   [
-    'title' => 'World news 2',
+    'title' => 'U.S news 2',
     'published' => '15.09.2025',
     'author' => 'Tamm',
-    'body' => 'Some world news body 2'
+    'body' => 'Some U.S news body 2'
   ],
   [
-    'title' => 'World news 3',
+    'title' => 'U.S news 3',
     'published' => '14.09.2025',
-    'author' => 'Hannes',
-    'body' => 'Some world news body 3'
+    'author' => 'No way',
+    'body' => 'Some U.S news body 3'
   ],
   [
-    'title' => 'World news 4',
+    'title' => 'U.S news 4',
     'published' => '13.09.2025',
     'author' => 'Hannes',
-    'body' => 'Some world news body 4'
+    'body' => 'Some U.S news body 4'
   ],
 ];
 ?>
-
 <?php include __DIR__ . '/partials/header.php'; ?>
-
-
     <main class="container">
-      <?php include __DIR__ . '/partials/hero.php'; ?>
-      <?php include __DIR__ . '/partials/featured.php'; ?>
-
       <div class="row g-5">
         <div class="col-md-8">
             <?php include __DIR__ . '/partials/posts.php'; ?>
