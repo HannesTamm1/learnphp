dd20cfaa9d93941aa59115e6db38c16ee9757abd
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-09-23 10:31:29 +0300

    Move posts to router

--- public/index.php
+++ public/index.php
@@ -1,18 +1,70 @@
 <?php
 
 if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
-    return false;    
+    return false;
 }
 
 // echo '<pre>';
 // var_dump($_SERVER);
 // echo '</pre>';
 
-switch($_SERVER['REQUEST_URI']) {
+switch ($_SERVER['REQUEST_URI']) {
     case '/':
+        $posts = [
+            [
+                'title' => 'World news 1',
+                'published' => '16.09.2025',
+                'author' => 'Hannes',
+                'body' => 'Some world news body 1'
+            ],
+            [
+                'title' => 'World news 2',
+                'published' => '15.09.2025',
+                'author' => 'Tamm',
+                'body' => 'Some world news body 2'
+            ],
+            [
+                'title' => 'World news 3',
+                'published' => '14.09.2025',
+                'author' => 'Hannes',
+                'body' => 'Some world news body 3'
+            ],
+            [
+                'title' => 'World news 4',
+                'published' => '13.09.2025',
+                'author' => 'Hannes',
+                'body' => 'Some world news body 4'
+            ],
+        ];
         include __DIR__ . '/../views/index.php';
         break;
     case '/us':
+        $posts = [
+            [
+                'title' => 'U.S news 1',
+                'published' => '16.09.2025',
+                'author' => 'Hannes',
+                'body' => 'Some U.S news body 1'
+            ],
+            [
+                'title' => 'U.S news 2',
+                'published' => '15.09.2025',
+                'author' => 'Tamm',
+                'body' => 'Some U.S news body 2'
+            ],
+            [
+                'title' => 'U.S news 3',
+                'published' => '14.09.2025',
+                'author' => 'No way',
+                'body' => 'Some U.S news body 3'
+            ],
+            [
+                'title' => 'U.S news 4',
+                'published' => '13.09.2025',
+                'author' => 'Hannes',
+                'body' => 'Some U.S news body 4'
+            ],
+        ];
         include __DIR__ . '/../views/us.php';
         break;
     default:
--- views/index.php
+++ views/index.php
@@ -1,32 +1,3 @@
-<?php
-$posts = [
-  [
-    'title' => 'World news 1',
-    'published' => '16.09.2025',
-    'author' => 'Hannes',
-    'body' => 'Some world news body 1'
-  ],
-  [
-    'title' => 'World news 2',
-    'published' => '15.09.2025',
-    'author' => 'Tamm',
-    'body' => 'Some world news body 2'
-  ],
-  [
-    'title' => 'World news 3',
-    'published' => '14.09.2025',
-    'author' => 'Hannes',
-    'body' => 'Some world news body 3'
-  ],
-  [
-    'title' => 'World news 4',
-    'published' => '13.09.2025',
-    'author' => 'Hannes',
-    'body' => 'Some world news body 4'
-  ],
-];
-?>
-
 <?php include __DIR__ . '/partials/header.php'; ?>
 
 
